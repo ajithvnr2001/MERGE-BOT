7d5da5abae9b67a789f711786c5846992ca60a27
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2022-12-16 18:38:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     OWNER_USERNAME = os.environ.get("ajithvnr")
     PASSWORD = os.environ.get("0000asdf")
     DATABASE_URL = os.environ.get("mongodb+srv://botvnr:BqyoAN11n7ilmhN9@cluster0.7ve9yva.mongodb.net/")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    LOGCHANNEL = os.environ.get("-1001616814441")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False

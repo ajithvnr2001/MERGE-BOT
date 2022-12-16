5ae27106b3613a57f76cc1846755abc81ddea04d
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2022-12-16 18:00:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    API_HASH = os.environ.get("9ff4a51876a555bdb530cd6a58baae87")
+    BOT_TOKEN = os.environ.get("5979051635:AAFXtaM-unQFsD8rHCXj8FMF9cDsDSPNzyQ")
+    TELEGRAM_API = os.environ["1954515"]
+    OWNER = os.environ.get("800555393")
+    OWNER_USERNAME = os.environ.get("ajithvnr")
+    PASSWORD = os.environ.get("0000asdf")
+    DATABASE_URL = os.environ.get("mongodb+srv://botvnr:BqyoAN11n7ilmhN9@cluster0.7ve9yva.mongodb.net/")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)

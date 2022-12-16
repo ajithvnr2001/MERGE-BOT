3d95713b7d002e59701dbf998794870efa011e93
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2022-12-16 20:00:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
-# Fill your config
-API_HASH = "9ff4a51876a555bdb530cd6a58baae87"
-BOT_TOKEN = "5979051635:AAFXtaM-unQFsD8rHCXj8FMF9cDsDSPNzyQ"
-TELEGRAM_API = "1954515"
-OWNER = "800555393"
-OWNER_USERNAME = "ajithvnr"
-PASSWORD = "0000asdf"
-DATABASE_URL = "mongodb+srv://botvnr:BqyoAN11n7ilmhN9@cluster0.7ve9yva.mongodb.net/"
-LOGCHANNEL = "1001616814441"
-USER_SESSION_STRING = "" # Premium account session string to upload upto 4GB (requires "LOGCHANNEL")
+import os
 
-# tired of redeploying :(
-UPSTREAM_REPO = "https://github.com/ajithvnr2001/MERGE-BOT"
-UPSTREAM_BRANCH = "master"
+
+class Config(object):
+    API_HASH = os.environ.get("9ff4a51876a555bdb530cd6a58baae87")
+    BOT_TOKEN = os.environ.get("5979051635:AAFXtaM-unQFsD8rHCXj8FMF9cDsDSPNzyQ")
+    TELEGRAM_API = os.environ["1954515"]
+    OWNER = os.environ.get("800555393")
+    OWNER_USERNAME = os.environ.get("ajithvnr")
+    PASSWORD = os.environ.get("0000asdf")
+    DATABASE_URL = os.environ.get("mongodb+srv://botvnr:BqyoAN11n7ilmhN9@cluster0.7ve9yva.mongodb.net/")
+    LOGCHANNEL = os.environ.get("-1001616814441")  # Add channel id as -100 + Actual ID
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
+    IS_PREMIUM = False
+    MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
